8d78aa5a25e668b6c47ca4298eba216ebf445176
CNMWDream <735789711@qq.com>
2019-07-19 11:23:21 +0800

    配置规则

--- cn.cloud.ribbon/src/main/java/cn/cloud/ribbon/balance/config/RibbonConfig.java
+++ cn.cloud.ribbon/src/main/java/cn/cloud/ribbon/balance/config/RibbonConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+
 @Configuration
 public class RibbonConfig {
 	
@@ -13,5 +16,11 @@ public class RibbonConfig {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+//    @Bean
+//    public IRule myRule(){
+//        //return new RoundRobinRule();//轮询
+//        //return new RetryRule();//重试
+//        return new BestAvailableRule();
+//    }
 
 }
