367e543f73768aa75e0eee6389fc55742e7aa7d2
CNMWDream <735789711@qq.com>
2019-07-26 15:21:17 +0800

    111

--- cn.cloud.ZconfiService/src/main/java/cn/cloud/ZconfiService/Application.java
+++ cn.cloud.ZconfiService/src/main/java/cn/cloud/ZconfiService/Application.java
@@ -1,16 +1,15 @@
-package cn.cloud.zuul;
+package cn.cloud.ZconfiService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableConfigServer
 @EnableEurekaClient
-@EnableZuulProxy
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
