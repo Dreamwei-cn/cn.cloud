51a33573113150894f6a15522f615e0e5a669d59
CNMWDream <33818203+CNMWDream@users.noreply.github.com>
2019-07-29 21:16:48 +0800

    11

--- cn.cloud.shop-api/cn.cloud.logistics-api/src/main/java/cn/cloud/logistics/LogisticsApplication.java
+++ cn.cloud.shop-api/cn.cloud.logistics-api/src/main/java/cn/cloud/logistics/LogisticsApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
-public class AppApplication {
+public class LogisticsApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(LogisticsApplication.class, args);
 		
 	}
 	
